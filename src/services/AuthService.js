98af206cd6969dae38e54b66b51951dc1b281e1a
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-13 18:32:51 -0400

    feat
    * Created register service method

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -39,6 +39,43 @@ class AuthService {
       throw error;
     }
   }
+
+  static async register({ fullname, email, password, confirmPassword }) {
+    try {
+      if (password !== confirmPassword) {
+        throw ({ status: 400, message: 'Passwords do not match' });
+      }
+
+      const user = await User.findOne({
+        where: {
+          email: email,
+        },
+      });
+
+      if (user) {
+        throw ({ status: 400, message: 'Email already exists' });
+      }
+
+      const userCreated = await User.create({
+        fullname,
+        email,
+        username: email
+      });
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await UserCredential.create({
+        password: hashedPassword,
+        userId: userCreated.id,
+      });
+
+      return userCreated;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
 }
 
 export default AuthService;
