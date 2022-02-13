cc69b29ae44a1df8e51ef55d159a3f792544f8e4
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-13 18:32:07 -0400

    feat
    * Created controller method to register users

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -15,6 +15,22 @@ class AuthController {
       });
     }
   }
+
+  static async register(req, res) {
+    const { fullname, email, password, confirmPassword } = req.body;
+    try {
+      const entityCreated = await AuthService.register({
+        fullname, email, password, confirmPassword
+      });
+      return res.json(Response.get('success', entityCreated));
+    } catch (error) {
+      res.status(error.status || 500).json({
+        message: error.message || 'Something goes wrong',
+        data: error
+      });
+    }
+  }
+
 }
 
 export default AuthController;
