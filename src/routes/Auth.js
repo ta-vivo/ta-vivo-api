4858deb8dd20e9164cd09602c2238bd5273ccf3a
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-13 18:32:38 -0400

    feat
    * Created register endpoint

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -4,5 +4,6 @@ import AuthController from '../controllers/AuthController';
 const router = Router();
 
 router.post('/login', AuthController.login);
+router.post('/register', AuthController.register);
 
 export default router;
