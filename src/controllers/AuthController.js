36f73c35599cd6889f20aa9c9a84e0905b1a673f
Albert Hidalgo <alhidalgodev@gmail.com>
2022-02-17 22:46:07 -0400

    feat
    * Created method to confirm email code

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -31,6 +31,20 @@ class AuthController {
     }
   }
 
+  static async registerEmailConfirmation(req, res) {
+    const { uniqueCode } = req.body;
+    const user = req.user;
+    try {
+      await AuthService.registerEmailConfirmation({ user, uniqueCode });
+      return res.json(Response.get('success', {}));
+    } catch (error) {
+      res.status(error.status || 500).json({
+        message: error.message || 'Something goes wrong',
+        data: error
+      });
+    }
+  }
+
 }
 
 export default AuthController;
