31f65c438b0088bfc37cac3630fd5c8dd3de6198
Albert Hidalgo <alhidalgodev@gmail.com>
2022-03-22 20:00:52 -0400

    feat
    * Created payments routes

---
+++ src/routes/Payment.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import PaymentController from '../controllers/PaymentController';
+import { verifyToken } from '../middlewares/Auth';
+
+const router = Router();
+
+router.get('/paypal-request-token', verifyToken, PaymentController.paypalRequestToken);
+
+export default router;
