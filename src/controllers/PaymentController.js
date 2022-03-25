03f92226cd7b45d28e4cd81972952693031b7bca
Albert Hidalgo <alhidalgodev@gmail.com>
2022-03-24 23:29:35 -0400

    feat
    * Created method to save paypal subscription

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -13,6 +13,18 @@ class PaymentController {
     }
   }
 
+  static async paypalSusbcription(req, res) {
+    try {
+      const { subscriptionId } = req.body;
+      const { user } = req;
+      const subscription = await PaypalService.paypalSubscription({ subscriptionId, user });
+
+      return res.json(Response.get('Subscription created', subscription));
+    } catch (error) {
+      return res.json(Response.get('Something goes wrong', error, 500));
+    }
+  }
+
 }
 
 export default PaymentController;
