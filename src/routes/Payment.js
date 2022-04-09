6fab091617f4c1c6356f6de3db8c36edf70c0abe
Albert Hidalgo <alhidalgodev@gmail.com>
2022-04-09 14:55:30 -0400

    feat
    * Created route to get subscription billing

--- src/routes/Payment.js
+++ src/routes/Payment.js
@@ -5,6 +5,8 @@ import { verifyToken } from '../middlewares/Auth';
 const router = Router();
 
 router.get('/paypal-request-token', verifyToken, PaymentController.paypalRequestToken);
+router.get('/paypal-transactions', verifyToken, PaymentController.paypalTransactions);
+
 router.post('/paypal-subscription', verifyToken, PaymentController.paypalSusbcription);
 router.post('/paypal-subscription-cancel', verifyToken, PaymentController.paypalSusbcriptionCancel);
 
