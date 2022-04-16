37ad1cf729876c459815687045296d59852072eb
Albert Hidalgo <alhidalgodev@gmail.com>
2022-04-16 08:26:59 -0400

    refactor
    * Removed unused model

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import userRoutes from '../routes/User';
-import TaskRoutes from '../routes/Task';
 import CheckRoutes from '../routes/Check';
 import IntegrationRoutes from '../routes/Integration';
 import Auth from '../routes/Auth';
@@ -13,7 +12,6 @@ let router = express.Router();
 
 // Routes
 router.use('/users', userRoutes);
-router.use('/tasks', TaskRoutes);
 router.use('/checks', CheckRoutes);
 router.use('/auth', Auth);
 router.use('/integrations', IntegrationRoutes);
