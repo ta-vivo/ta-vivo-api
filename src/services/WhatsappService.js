ae68986133859ded968212b68a60fb8e0d632a26
Albert Hidalgo <alhidalgodev@gmail.com>
2022-07-28 20:37:14 -0400

    feat
    * Created whatsapp service

---
+++ src/services/WhatsappService.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+class WhatsappService {
+
+
+  /**
+   * It creates an axios instance with the base URL of the Whatsapp service and the authorization token
+   * @returns An axios instance
+   */
+  static getAxiosInstance() {
+    let instance;
+    if (process.env.ENVIRONMENT !== 'production') {
+      instance = axios.create({
+        baseURL: process.env.WHATSAPP_SERVICE_API,
+        headers: {
+          'Content-Type': 'application/json',
+          'authorization': `Bearer ${process.env.WHATSAPP_SERVICE_API_TOKEN}`
+        }
+      });
+
+    } else {
+      instance = {
+        post: () => {
+          // eslint-disable-next-line no-undef
+          return new Promise(resolve => {
+            resolve('fake success');
+          });
+        },
+      };
+      console.log('⚠️ In "development" mode the Audit service is disabled');
+    }
+
+    return instance;
+  }
+
+
+  static async sendMessage({ phone, message }) {
+    try {
+      return this.getAxiosInstance()
+        .post('/messages/send-message', {
+          phone: phone,
+          message: message
+        })
+        .catch((error) => {
+          console.log(error.response);
+          throw { message: 'Failed when trying send Whatsapp message' };
+        });
+    } catch (error) {
+      throw error;
+    }
+  }
+
+}
+
+export default WhatsappService;
