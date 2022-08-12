87341e89c961fcbb8fa8162fa85964d6f1c4fec2
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-11 22:11:20 -0400

    refactor
    * Changed property names and added header name

--- src/models/CheckAuthorization.js
+++ src/models/CheckAuthorization.js
@@ -8,13 +8,17 @@ const CheckAuthorization = sequelize.define('check_authorizations', {
     primaryKey: true,
     autoIncrement: true
   },
-  encryptedData: {
+  headerName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  encryptedToken: {
     type: Sequelize.JSONB,
     allowNull: false
-  }
+  },
 }, {});
 
 CheckAuthorization.belongsTo(Checks);
-Checks.hasMany(CheckAuthorization);
+Checks.hasMany(CheckAuthorization, { onDelete: 'cascade', hook: true });
 
 export default CheckAuthorization;
