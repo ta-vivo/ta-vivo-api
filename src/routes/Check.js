7d9830d262214166a7c542095a2d04201f869667
Albert Hidalgo <alhidalgodev@gmail.com>
2022-08-22 21:32:06 -0400

    feat
    * Created disable route to handle the disable status for checks

--- src/routes/Check.js
+++ src/routes/Check.js
@@ -8,6 +8,7 @@ router.get('/', verifyToken, CheckController.getAll);
 router.get('/:id', verifyToken, CheckController.getById);
 router.get('/:id/logs', verifyToken, CheckController.getLogsByCheckId);
 router.put('/:id', verifyToken, CheckController.update);
+router.put('/:id/disable', verifyToken, CheckController.disable);
 router.post('/', verifyToken, CheckController.create);
 router.delete('/:id', verifyToken, CheckController.delete);
 
